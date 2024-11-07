9ca60959c928a7619293e166855c24719d3717a8
iAmBrig12 <Bthornock12@gmail.com>
2024-11-07 11:28:55 -0600

    added save eval option

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from NetClassifier import Net
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix  # Import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 import DataPreprocess
+import sys
+import pandas as pd
+
+save_eval = False
+if len(sys.argv) > 1 and int(sys.argv[1]):
+    save_eval = True
+    eval_df = pd.DataFrame(columns=['Lablel', 'Accuracy', 'Precision', 'Recall', 'F1 Score', 'Confusion Matrix'])
 
 if __name__ == '__main__':
     # Step 1: Load and preprocess the data
@@ -24,24 +31,38 @@ if __name__ == '__main__':
     labels = ['ARP Spoofing', 'Benign', 'DNS Flood', 'Dictionary Attack',
        'ICMP Flood', 'OS Scan', 'Ping Sweep', 'Port Scan', 'SYN Flood',
        'Slowloris', 'UDP Flood', 'Vulnerability Scan']
+    
     for i in range(output_size):
         acc = accuracy_score(y_test[:, i], y_pred_binary[:, i])
         prec = precision_score(y_test[:, i], y_pred_binary[:, i], zero_division=0)
         rec = recall_score(y_test[:, i], y_pred_binary[:, i], zero_division=0)
         f1 = f1_score(y_test[:, i], y_pred_binary[:, i])
+        cm = confusion_matrix(y_test[:, i], y_pred_binary[:, i])
 
         print(f'{labels[i]}:')
         print(f'Accuracy: {acc}')
         print(f'Precision: {prec}')
         print(f'Recall: {rec}')
         print(f'F1 Score: {f1}')
-        print(f'Confusion Matrix:\n{confusion_matrix(y_test[:, i], y_pred_binary[:, i])}')
+        print(f'Confusion Matrix:\n{cm}')
         print()
 
+        if save_eval:
+            eval_df = eval_df.append({'Label': labels[i], 'Accuracy': acc, 'Precision': prec, 'Recall': rec, 'F1 Score': f1, 'Confusion Matrix': cm}, ignore_index=True)
+
     # Overall evaluation
+    acc = accuracy_score(y_test, y_pred_binary)
+    prec = precision_score(y_test, y_pred_binary, average="weighted", zero_division=0)
+    rec = recall_score(y_test, y_pred_binary, average="weighted", zero_division=0)
+    f1 = f1_score(y_test, y_pred_binary, average="weighted")
+
     print('Overall:')
-    print(f'Accuracy: {accuracy_score(y_test, y_pred_binary)}')
-    print(f'Precision: {precision_score(y_test, y_pred_binary, average="weighted", zero_division=0)}')
-    print(f'Recall: {recall_score(y_test, y_pred_binary, average="weighted", zero_division=0)}')
-    print(f'F1 Score: {f1_score(y_test, y_pred_binary, average="weighted")}')
+    print(f'Accuracy: {acc}')
+    print(f'Precision: {prec}')
+    print(f'Recall: {rec}')
+    print(f'F1 Score: {f1}')
+
+    if save_eval:
+        eval_df = eval_df.append({'Label': 'Overall', 'Accuracy': acc, 'Precision': prec, 'Recall': rec, 'F1 Score': f1, 'Confusion Matrix': 'n/a'}, ignore_index=True)
+        eval_df.to_csv('evaluation.csv', index=False)
 
